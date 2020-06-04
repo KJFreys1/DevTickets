c47e2e49a738bfc6fe1a04e16d81f59532790feb
KJFreys1 <kfreyermuth20@gmail.com>
2020-06-03 17:31:06 -0700

    begin revamp for auth: init auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,91 @@
-import React, { useState, useEffect } from "react";
-import { Route, Redirect } from "react-router-dom"
-import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import { Route, Redirect } from "react-router-dom"
+// import axios from "axios";
 
-import LandingPage from "./components/LandingPage"
-import Login from "./components/Register/Login"
-import SignUp from "./components/Register/SignUp"
-import Profile from "./components/Main/Profile/Profile"
+// import LandingPage from "./components/LandingPage"
+// import Login from "./components/Register/Login"
+// import SignUp from "./components/Register/SignUp"
+// import Profile from "./components/Main/Profile/Profile"
 
-import "./App.css";
+// import "./App.css";
+
+// function App() {
+//   let [user, setUser] = useState()
+
+//   const BASEURL = "http://dev-tickets.herokuapp.com"
+
+//   useEffect(() => {
+//     if (localStorage.token) {
+//       axios.get(BASEURL + '/login/user', {
+//         headers: {
+//           "x-auth-token": localStorage.token
+//         }
+//       }).then(res => {
+//         setUser(res.data)
+//       })
+//     }
+//   }, [])
+
+//   const register = user => {
+//     axios.post(BASEURL+"/register", user).then(res => {
+//       localStorage.token = res.data.token
+//       setUser(res.data.user)
+//     }).catch(err => {
+//       console.log(err)
+//     })
+//   }
+
+//   const login = user => {
+//     axios.post(BASEURL+"/login", user).then(res => {
+//       localStorage.token = res.data.token
+//       setUser(res.data.user)
+//     }).catch(err => {
+//       console.log(err)
+//     })
+//   }
+
+//   const logout = () => {
+//     localStorage.clear()
+//     setUser()
+//   }
+
+//   const redirectPath = user ? <Redirect to="/profile" /> : null
+
+//   return (
+//     <div>
+//       {redirectPath}
+//       <Route path="/" exact render={props => <LandingPage {...props} />} />
+//       <Route path="/login" render={props => <Login {...props} user={user} login={login} />} />
+//       <Route path="/register" render={props => <SignUp {...props} user={user} register={register} />} />
+//       <Route path="/profile" render={props => <Profile {...props} logout={logout} user={user} />} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/App.js
+
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+import history from "./utils/history";
+// import { useAuth0 } from "./react-auth0-spa";
 
 function App() {
-  let [user, setUser] = useState()
-
-  const BASEURL = "http://dev-tickets.herokuapp.com"
-
-  useEffect(() => {
-    if (localStorage.token) {
-      axios.get(BASEURL + '/login/user', {
-        headers: {
-          "x-auth-token": localStorage.token
-        }
-      }).then(res => {
-        setUser(res.data)
-      })
-    }
-  }, [])
-
-  const register = user => {
-    axios.post(BASEURL+"/register", user).then(res => {
-      localStorage.token = res.data.token
-      setUser(res.data.user)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  const login = user => {
-    axios.post(BASEURL+"/login", user).then(res => {
-      localStorage.token = res.data.token
-      setUser(res.data.user)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  const logout = () => {
-    localStorage.clear()
-    setUser()
-  }
-
-  const redirectPath = user ? <Redirect to="/profile" /> : null
-
   return (
-    <div>
-      {redirectPath}
-      <Route path="/" exact render={props => <LandingPage {...props} />} />
-      <Route path="/login" render={props => <Login {...props} user={user} login={login} />} />
-      <Route path="/register" render={props => <SignUp {...props} user={user} register={register} />} />
-      <Route path="/profile" render={props => <Profile {...props} logout={logout} user={user} />} />
+    <div className="App">
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App';
-import "./index.css"
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import App from './App';
+// import "./index.css"
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+// ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
