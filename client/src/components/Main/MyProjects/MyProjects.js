d820e8138fd05190f4842ad3af0b80cd6e0e4c28
KJFreys1 <kfreyermuth20@gmail.com>
2020-07-25 12:41:07 -0700

    fixed display projs on myprojects and added form to post new project

--- client/src/components/Main/MyProjects/MyProjects.js
+++ client/src/components/Main/MyProjects/MyProjects.js
@@ -3,13 +3,70 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from "../../../react-auth0-spa";
 import axios from 'axios'
+import './MyProjects.css'
 
 import Loading from '../../Loading'
 
 export default function MyProjects(props) {
     const { user } = useAuth0()
+    let [projListElem, setProjListElem] = useState([])
+    let [displayForm, setDisplayForm] = useState("none")
+    let [projTitle, setProjTitle] = useState("")
+    let [projDesc, setProjDesc] = useState("")
 
     const BASEURL = "http://dev-tickets.herokuapp.com"
+
+    const createProjListElement = proj => {
+        return (
+            <div className="proj-list-elem" key={proj._id}>
+                <h1 className="proj-list-name">{proj.name}</h1>
+                <button className="proj-list-btn">Details</button>
+                <button className="proj-list-btn">Tickets</button>
+            </div>
+        )
+    }
+
+    const getProjects = () => {
+        axios.get(BASEURL + `/project/uid/${props.devUser._id}`).then(projs => {
+            setProjListElem(projs.data.map(proj => {
+                return createProjListElement(proj)
+            }))
+        })
+    }
+
+    const handleDisplayFormSwitch = () => {
+        if (displayForm === "none") {
+            setDisplayForm("block")
+        } else {
+            setDisplayForm("none")
+        }
+    }
+
+    const handleProjTitleChange = e => {
+        setProjTitle(e.target.value)
+    }
+
+    const handleProjDescChange = e => {
+        setProjDesc(e.target.value)
+    }
+
+    const handleFormSubmit = e => {
+        e.preventDefault()
+        let newProjData = { 
+            name: projTitle, 
+            description: projDesc, 
+            managers: [props.devUser._id] 
+        }
+        newProject(newProjData)
+        setProjTitle("")
+        setProjDesc("")
+    }
+
+    const newProject = data => {
+        axios.post(BASEURL + `/project/${props.devUser._id}`, data).then(proj => {
+            setProjListElem(projListElem.push(createProjListElement(proj)))
+        }).catch(err => console.log(err))
+    }
 
     useEffect(() => {
         if (user && !props.devUser) {
@@ -18,28 +75,17 @@ export default function MyProjects(props) {
                     props.setDevUser(existingUser.data)
                 }
             })
+        } else if (props.devUser) {
+            getProjects()
         }
     }, [user])
-
+    
     if (!props.devUser) {
         return <Loading />
     }
 
-    let projList = props.devUser.projects_managed
-    projList.concat(props.devUser.projects_joined)
-
-    let projListElem = null
-    if (projList.length > 0) {
-        projList.map(proj => {
-            console.log(proj)
-            return (
-                <div className="proj-list-elem">
-                    <h1>{proj.name}</h1>
-                    <button>Details</button>
-                    <button>Tickets</button>
-                </div>
-            )
-        })
+    if (!projListElem[0]) {
+        getProjects()
     }
 
     return (
@@ -48,6 +94,12 @@ export default function MyProjects(props) {
             <div className="proj-list">
                 {projListElem}
             </div>
+            <button onClick={handleDisplayFormSwitch}>New project</button>
+            <form onSubmit={handleFormSubmit} style={{display: displayForm}}>
+                <input value={projTitle} onChange={handleProjTitleChange}></input>
+                <textarea value={projDesc} onChange={handleProjDescChange}></textarea>
+                <button type="submit">Submit</button>
+            </form>
             <Link to="/profile">Back to Profile</Link>
         </div>
     )
